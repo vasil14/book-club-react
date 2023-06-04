13476771aadfcfe26a21ff8df2943b89e72d0380
vasil14 <vasil_v@hotmail.com>
2023-06-04 18:41:47 +0200

    regster added

--- src/components/User/Register.tsx
+++ src/components/User/Register.tsx
@@ -1,12 +1,15 @@
-import useAuthContext from '../../context/AuthContext';
-import { FormEvent, useState } from 'react';
-import { Link } from 'react-router-dom';
+import useAuthContext from "../../context/AuthContext";
+import { FormEvent, useState } from "react";
+import { Link } from "react-router-dom";
+import { ErrorMessage, FormContainer, FormStyled, LogoImg, Row } from "./AuthFormStyled";
+import { ButtonStyled } from "../ButtonStyled";
+
 
 const Register = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [password_confirmation, setPasswordConfirmation] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [password_confirmation, setPasswordConfirmation] = useState("");
 
   const { register, errors }: any = useAuthContext();
 
@@ -15,7 +18,58 @@ const Register = () => {
     register({ name, email, password, password_confirmation });
   };
 
-  return <div></div>;
+  return (
+    <FormContainer>
+      <Link to={"/"}>
+        <LogoImg src="/bookclub-logo.png" alt="logo" />
+      </Link>
+      <FormStyled action="POST" onSubmit={handleRegister}>
+        <Row>
+          <label htmlFor="name">Name</label>
+          <input
+            type="name"
+            placeholder="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          {errors?.name && <ErrorMessage>{errors.name}</ErrorMessage>}
+        </Row>
+        <Row>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          {errors?.email && <ErrorMessage>{errors.email}</ErrorMessage>}
+        </Row>
+        <Row>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {errors?.password && <ErrorMessage>{errors.password}</ErrorMessage>}
+        </Row>
+        <Row>
+          <label htmlFor="password_confirmation">Password Confirmation</label>
+          <input
+            type="password"
+            placeholder="Password confirmation"
+            value={password_confirmation}
+            onChange={(e) => setPasswordConfirmation(e.target.value)}
+          />
+          {errors?.password_confirmation && (
+            <ErrorMessage>{errors.password_confirmation}</ErrorMessage>
+          )}
+        </Row>
+        <ButtonStyled>Register</ButtonStyled>
+      </FormStyled>
+    </FormContainer>
+  );
 };
 
 export default Register;
