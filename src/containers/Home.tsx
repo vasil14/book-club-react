64f0cb422077514542c830aa1c6d2aac008f987c
vasil-vangjeli <vasil_v@hotmail.com>
2023-06-15 17:43:18 +0200

    trying node api

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import useAuthContext from "../context/AuthContext";
 
 const Home = () => {
+  const [first, setfirst]: any = useState()
+  fetch('http://localhost:3000/').then(res=> res.json()).then(data=>setfirst(data?.message))
   const {user}: any = useAuthContext();
+  console.log(first);
   
-  return <>Welcome, {user?.name}</>;
+  return <>Welcome, {first}</>;
 };
 
 export default Home;
